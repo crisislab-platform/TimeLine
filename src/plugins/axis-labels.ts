dffe92bc126c64ffc1b77bafbdad9e1836494993
Zade Viggers <zade@viggers.net>
2023-07-14 20:11:52 +1200

    Fix axis labels spacing

--- src/plugins/axis-labels.ts
+++ src/plugins/axis-labels.ts
@@ -12,18 +12,21 @@ export const axisLabelPlugin = (): TimeLinePlugin => ({
 		styleTag: document.createElement("style"),
 	},
 	construct: function (chart) {
-		chart.leftPadding += 30;
+		chart.leftPadding += 15;
+		chart.bottomPadding += 10;
 
-		this.data.styleTag.innerText = `
-        .crisislab-timeline-axis-label {
-				font-size: 12px;
+		this.data.styleTag.innerText = `.crisislab-timeline-axis-label {
+				font-size: 16px;
 				position: absolute;
 				user-select: none;
+				font-family: Arial, sans-serif;
 			}
 			.crisislab-timeline-axis-label.crisislab-timeline-x-axis {
 				left: 50%;
 				transform: translateX(-50%);
 				bottom: 0px;
+				margin-bottom: 2px;
+
 			}
 
 			.crisislab-timeline-axis-label.crisislab-timeline-y-axis {
@@ -31,6 +34,7 @@ export const axisLabelPlugin = (): TimeLinePlugin => ({
 				top: 50%;
 				writing-mode: vertical-rl;
 				transform: rotate(180deg) translateY(50%);
+				margin-left: 2px;
 			}`;
 		chart.container.appendChild(this.data.styleTag);
 
