85838173c2e3fed3c3c2a1130dddb91f0f854add
Zade Viggers <zade@viggers.net>
2024-01-25 22:35:56 +1300

    Add basic staic example

---
+++ examples/basic-static/basic-static.ts
@@ -0,0 +1,47 @@
+import {
+	TimeLine,
+	timeAxisPlugin,
+	axisLabelPlugin,
+	TimeLineDataPoint,
+} from "../../src/index";
+
+const timeWindow = 30 * 1000; // 30 seconds
+
+const data: TimeLineDataPoint[] = [];
+const chart = new TimeLine({
+	container: document.getElementById("chart-container") as HTMLElement,
+	data,
+	timeWindow,
+	// Note that these aren't used by the chart itself, they're just used by plugins
+	timeAxisLabel: "Time",
+	valueAxisLabel: "Random numbers",
+	plugins: [
+		// By default, the chart doesn't draw an time or y axis.
+		// You can use these built-in plugins though.
+		timeAxisPlugin((time) => new Date(time).toLocaleTimeString()),
+		axisLabelPlugin(),
+	],
+});
+
+// @ts-ignore
+window.chart = chart;
+
+let prev = 0;
+for (let i = 0; i < 1000; i++) {
+	const value =
+		prev + Math.floor(Math.random() * 10) * (Math.random() > 0.5 ? -1 : 1);
+	prev = value;
+	data.push({
+		time: Date.now() + i * 100,
+		value,
+	});
+}
+// Call chart.recompute() when you're done updating `data`
+chart.recompute();
+
+// Note that you need to call chart.draw() yourself
+function renderLoop() {
+	requestAnimationFrame(renderLoop);
+	chart.draw();
+}
+renderLoop();
