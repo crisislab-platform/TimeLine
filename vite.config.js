0e176a281092fb5c09c9165be33dd29610eb1ddc
Zade Viggers <zade@viggers.net>
2024-01-26 12:33:14 +1300

    Get examples ready to publish

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 // @ts-check
+import "dotenv/config";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const buildSiteMode = process.env.BUILD_SITE_NOT_LIBRARY === "yes";
 
 export default defineConfig({
 	plugins: [dts()],
@@ -13,11 +16,13 @@ export default defineConfig({
 		include: ["examples/**/*"],
 	},
 	build: {
-		lib: {
-			entry: resolve(__dirname, "src/index.ts"),
-			name: "TimeLine",
-			fileName: "TimeLine",
-			formats: ["es", "umd"],
-		},
+		lib: buildSiteMode
+			? undefined
+			: {
+					entry: resolve(__dirname, "src/index.ts"),
+					name: "TimeLine",
+					fileName: "TimeLine",
+					formats: ["es", "umd"],
+				},
 	},
 });
