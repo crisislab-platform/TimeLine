e68944bde41397d6fb569f32d249a07fcd81f77f
Zade Viggers <zade@viggers.net>
2025-02-27 19:01:20 +1300

    Markers seperate demo

---
+++ examples/markers/markers.ts
@@ -0,0 +1,77 @@
+// @ts-nocheck
+
+import {
+	TimeLine,
+	timeAxisPlugin,
+	axisLabelPlugin,
+	TimeLineDataPoint,
+} from "../../src/index";
+
+const timeWindow = 30 * 1000; // 30 seconds
+
+const data: TimeLineDataPoint[] = [];
+const chart = new TimeLine({
+	container: document.getElementById("chart-container") as HTMLElement,
+	data,
+	timeWindow,
+	// Note that these aren't used by the chart itself, they're just used by plugins
+	timeAxisLabel: "Time",
+	valueAxisLabel: "Random numbers",
+	plugins: [
+		// By default, the chart doesn't draw an time or y axis.
+		// You can use these built-in plugins though.
+		axisLabelPlugin(),
+	],
+	padding: {
+		top: 5,
+		left: 5,
+		bottom: 5,
+		right: 5,
+	},
+	markers: [
+		{
+			time: Date.now() + 3000,
+			// alwaysShow: true,
+			label: "Banana",
+			labelSide: "before",
+			colour: "red",
+		},
+		{
+			time: Date.now() - 3000,
+			alwaysShow: true,
+			label: "Carrot",
+			labelSide: "after",
+			colour: "orange",
+			lineStyle: "dotted",
+		},
+		{
+			value: 10,
+			alwaysShow: true,
+			label: "Avocado",
+			colour: "rebeccapurple",
+			lineStyle: "solid",
+		},
+	],
+});
+
+window.chart = chart;
+window.data = data;
+
+let prev = 0;
+setInterval(() => {
+	const value =
+		prev + Math.floor(Math.random() * 10) * (Math.random() > 0.5 ? -1 : 1);
+	prev = value;
+	data.push({
+		time: Date.now(),
+		value,
+	});
+
+	// Avoid filling up ram too much
+	if (data.length > 10000) {
+		data.shift();
+	}
+
+	// Call chart.recompute() when you're done updating `data`
+	chart.recompute();
+}, 100);
